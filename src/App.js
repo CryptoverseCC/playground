05c11a392de4aa84a12d6862603dc79b7c5a3822
Grzegorz Kapkowski <grzegorz.kapkowski@gmail.com>
2018-04-15 16:47:30 +0200

    Slightly better look

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Paper from 'material-ui/Paper';
+import Button from 'material-ui/Button';
+import TextField from 'material-ui/TextField';
+import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import Collapse from 'material-ui/transitions/Collapse';
+import IconButton from 'material-ui/IconButton';
+import FavoriteIcon from '@material-ui/icons/Favorite';
 import './App.css';
 
 const API = "http://localhost:8000"
 
 
 class Form extends Component {
+
+  style = {
+      container: {
+        padding: 20
+      },
+      response: {
+        padding: 20,
+        marginTop: 20
+      },
+      input: {
+        display: 'block'
+      }
+  }
 
   constructor(params) {
     super()
@@ -45,38 +64,48 @@ class Form extends Component {
   }
 
   render() {
-    if (this.spec.params) {
-      return <div>
-        <h3>Params:</h3>
-        {
-          Object.keys(this.spec.params).map(param => <div key={param}>
-            {param} (required: {this.spec.params[param] ? "True": "False"})
-            <input type="text" onChange={this.onChange(param)} value={this.state.params[param]}/>
-            </div>)}
-        <button onClick={this.fetch.bind(this)}>Run</button>
-        <br/>
-        Response: {JSON.stringify(this.state.response)}
-      </div>
-    }
-
-    return <div>Missing specification</div>;
+    return <Paper style={this.style.container}>
+      <strong>Playground</strong>
+      <form noValidate autoComplete="off"> 
+        {Object.keys(this.spec.params || {}).map(param =>
+          <TextField
+            style={this.style.input}
+            required={this.spec.params[param]}
+            id={param}
+            label={param}
+            value={this.state.params[param]}
+            onChange={this.onChange(param)}
+            margin="normal"
+          />)}
+      </form>
+      <Button variant="raised" color="primary" onClick={this.fetch.bind(this)}>Run</Button>
+      <Paper style={this.style.response}>
+        <pre>{JSON.stringify(this.state.response, null, 2)}</pre>
+      </Paper>
+    </Paper>    
   }
 }
 
 
 const Algo = function Algo (params) {
-  
-
-  
-
-  return <div>
-    <h2>{params.data.name}</h2>
-    <pre>{params.data.docs}</pre>
-    <Form algo={params.data.name} spec={params.data.spec}/>
-  </div>;
+   return <Card>
+    <CardHeader
+      title={`${params.data.name}`}
+    />
+    <CardContent>
+      <pre>{params.data.docs}</pre>
+      <Form algo={params.data.name} spec={params.data.spec}/>
+    </CardContent>
+  </Card>;
 }
 
 class App extends Component {
+
+  style = {
+    title: {
+      padding: 20
+    }
+  }
 
   constructor() {
     super();
@@ -93,23 +122,16 @@ class App extends Component {
   }
 
   async getAlgortihms() {
-    const response = await fetch(`${API}/algos`);
+    const response = await fetch(`${API}/experimental_algos`);
     return await response.json(); 
   }
 
   render() {
     console.log(this.state);
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Userfeeds Playground</h1>
-        </header>
-        <ul>
-        {Object.keys(this.state.algorithms).map(key => <li key={key}><Algo data={this.state.algorithms[key]}/></li>)}
-        </ul>
-      </div>
-    );
+    return <Paper>
+      <h2 style={this.style.title}>Algorithms available on Userfeeds Platform</h2>
+      {Object.keys(this.state.algorithms).map(key => <Algo data={this.state.algorithms[key]}/>)}
+    </Paper>
   }
 }
 
